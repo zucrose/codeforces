9c8721c70a6a414843b545420ea7fe3256c4d65f
zucrose <uzrhmdkhn@gmail.com>
2021-07-27 10:55:00 +0530

    css added

--- app.js
+++ app.js
@@ -4,30 +4,49 @@ const https=require("https");
 const ejs=require("ejs");
 const { urlencoded } = require("body-parser");
 const app=express();
-app.set('view engine' ,'ejs')
+app.set('view engine' ,'ejs');
+app.use(express.static(__dirname + '/public/'));
 app.use(express.urlencoded({extended:true}));
 app.post("/",(req,res)=>{
     const user1=req.body.user1;
     const user2=req.body.user2;
     let userobj1,userobj2;
     https.get("https://codeforces.com/api/user.rating?handle="+user1,(response)=>{
+      
         response.on("data",(data)=>{
+
           userobj1=JSON.parse(data);
-          https.get("https://codeforces.com/api/user.rating?handle="+user2,(response1)=>{
-            response1.on("data",(data)=>{
-             userobj2=JSON.parse(data);
-           const arr=[];
-           userobj2.result.forEach(element => {
-             //   console.log(element.contestId);
-                const bruh=userobj1.result.map(x=>x.contestId).indexOf(element.contestId);
-                if(bruh!=-1)
-                arr.push( { cName:element.contestName,frank:userobj1.result[bruh].rank,srank:element.rank});
-            });
-            
+          if(userobj1.status=="OK")
+          {
+            https.get("https://codeforces.com/api/user.rating?handle="+user2,(response1)=>{
+              response1.on("data",(data)=>{
+                userobj2=JSON.parse(data);
+                if(userobj2.status=="OK"){
+                  const arr=[];
+                  userobj2.result.forEach(element => {
+                  //   console.log(element.contestId);
+                      const bruh=userobj1.result.map(x=>x.contestId).indexOf(element.contestId);
+                      if(bruh!=-1)
+                      arr.push( { cName:element.contestName,frank:userobj1.result[bruh].rank,srank:element.rank});
+                  });
                   
-            res.render("result",{user1:user1,user2:user2 ,arr:arr});
-         })
-        })
+                  res.locals.title = "comparision"; 
+                  res.render("result",{user1:user1,user2:user2 ,arr:arr});
+                }
+                else
+                {
+                   res.locals.title="CF comparator";
+                   res.render("home",{checker:user2})
+                }
+               
+              })
+            })
+         }
+         else
+         {
+          res.locals.title="CF comparator";
+           res.render("home",{checker:user1})
+         }
         })
     })
    
@@ -37,7 +56,8 @@ app.post("/",(req,res)=>{
    
    
 app.get("/",(req,res)=>{
-    res.render("home");
+  res.locals.title="CF comparator"
+    res.render("home",{checker:"true"});
 
 })
 app.listen(3000,()=>{console.log("server running")})
